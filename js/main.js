53a4f126db25d838913c5b7a552ac79ec4028337
Abhinraj <abhinrajpb88@gmail.com>
2022-03-17 14:09:36 +0530

    styles changed

--- js/main.js
+++ js/main.js
@@ -97,3 +97,11 @@ jQuery(function ($) {
   }
 });
 
+$(document).on("scroll", function () {
+	if ($(document).scrollTop() > 100) {
+		$("header").addClass("small");
+	} else {
+		$("header").removeClass("small");
+	}
+});
+
